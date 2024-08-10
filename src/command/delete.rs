d23132c8e1855830aeceeaade38ba6392812c8a6
Tomoyuki Hatakeyama <28624172+tsumli@users.noreply.github.com>
2024-08-10 16:13:51 +0900

    fix: error handling (#2)

--- src/command/delete.rs
+++ src/command/delete.rs
@@ -10,10 +10,12 @@ fn delete_branch(repo: &git2::Repository, branch: &str) -> Result<(), Box<dyn st
 
 pub fn delete() -> Result<(), Box<dyn std::error::Error>> {
     let repo = git2::Repository::open(".").unwrap();
-    let branches_string = get_branch_names_from_repository(&repo).expect("Failed to get branches");
+    let branches_string =
+        get_branch_names_from_repository(&repo, false).expect("Failed to get branches");
     let branches_str: Vec<&str> = branches_string.iter().map(AsRef::as_ref).collect();
     let branch_to_delete =
-        crate::ui::multi_select_from_options("Select a branch to delete to", &branches_str);
+        crate::ui::multi_select_from_options("Select a branch to delete to", &branches_str)
+            .expect("Failed to get branch");
 
     for branch in branch_to_delete {
         delete_branch(&repo, &branch).expect(&format!("Failed to delete branch: {}", branch));
